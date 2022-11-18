515b5bfb5ed5f77843232ae14db9d9875fb26bea
Kelly <kelly@fimpact.tech>
2022-11-18 17:37:02 +0900

    grouping

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,8 @@
-alert("hi");
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName){
+        console.log(" hello! " + otherPersonsName + " nice to meet you ");
+    }
+};
+
+player.sayHello("lynn");
