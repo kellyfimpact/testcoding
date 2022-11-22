bedca324f61bda0e4eb35421aeb978c858e7e9a7
Kelly <kelly@fimpact.tech>
2022-11-22 15:44:10 +0900

    momentum app

--- momentum/app.js
+++
@@ -1,8 +0,0 @@
-const player = {
-    name: "nico",
-    sayHello: function(otherPersonsName){
-        console.log(" hello! " + otherPersonsName + " nice to meet you ");
-    }
-};
-
-player.sayHello("lynn");
